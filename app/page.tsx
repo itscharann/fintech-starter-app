957317193c39d49c35e74eb8eabbca8f664760e8
Jonathan <jonathan.temp@paella.dev>
2025-04-09 16:24:44 -0700

    Cleanup (#10)
    * Add usdc faucet, improve ux
    
    * cleanup
    
    * removed dead code

--- app/page.tsx
+++ app/page.tsx
@@ -67,49 +67,4 @@ export default function Home() {
       </footer>
     </div>
   );
-
-  return (
-    <div className="mx-auto h-screen flex flex-col">
-      {/* <HomeContent /> */}
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <div className="flex w-full items-center md:justify-center gap-4 md:gap-12 flex-col md:flex-row">
-          <a
-            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-            href="https://github.com/Crossmint/solana-smart-wallet-quickstart"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image src="/file.svg" alt="GitHub" width={20} height={20} />
-            View code
-          </a>
-          <a
-            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-            href="https://github.com/crossmint"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image src="/window.svg" alt="GitHub" width={20} height={20} />
-            See all quickstarts
-          </a>
-          <a
-            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-            href="https://github.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image src="/globe.svg" alt="GitHub" width={20} height={20} />
-            Go to Crossmint.com
-          </a>
-        </div>
-        <div className="flex">
-          <Image
-            src="/crossmint-leaf.svg"
-            alt="Powered by Crossmint"
-            width={152}
-            height={100}
-          />
-        </div>
-      </footer>
-    </div>
-  );
 }
